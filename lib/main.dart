0db337346bb355fb04488e6f5b0c5e6e02b42bb8
asad bilal <asadb1817@gmail.com>
2021-06-29 17:05:17 +0500

    chat app

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+
+import 'package:asdooo_chat/helper/authenticate.dart';
+import 'package:asdooo_chat/helper/helperfunctions.dart';
+import 'package:asdooo_chat/views/chatrooms.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool userIsLoggedIn;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async {
+    await HelperFunctions.getUserLoggedInSharedPreference().then((value){
+      setState(() {
+        userIsLoggedIn  = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xffFC6011),
+        scaffoldBackgroundColor: Color(0xff1F1F1F),
+        accentColor: Colors.cyanAccent,
+        fontFamily: "OverpassRegular",
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: userIsLoggedIn != null ?  userIsLoggedIn ? ChatRoom() : Authenticate()
+          : Container(
+        child: Center(
+          child: Authenticate(),
+        ),
+      ),
+    );
+  }
+}
